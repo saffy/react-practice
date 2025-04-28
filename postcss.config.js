3d79e798a78738fc25c84e815196c6c245152349
Rose Grundy <me@roseanna.nz>
2025-04-28 12:14:53 +0100

    Update tailwindcss and postcss configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-} 
+// postcss.config.js
+export default {
+	plugins: {
+		"postcss-import": {},
+		"@tailwindcss/postcss": {},
+		autoprefixer: {},
+	},
+};
+1
